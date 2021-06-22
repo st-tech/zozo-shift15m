daf2fc765b445877fa7ef68d935029993ec00414
Masanari Kimura <noconoco.lib@gmail.com>
2021-06-23 00:20:16 +0900

    bugfix

--- tests/datasets/test_numlikes_tabular.py
+++ tests/datasets/test_numlikes_tabular.py
@@ -1,6 +1,6 @@
 import unittest
 import shift28m.msgs as M
-from shift28m.datasets import load_numlikes_tabular
+from shift28m.datasets import NumLikesRegression
 
 
 class TestNumlikesTabular(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestNumlikesTabular(unittest.TestCase):
 
     def test_dataset_corrupted(self):
         with self.assertRaises(RuntimeError, msg=M.DATASET_NOT_FOUND):
-            load_numlikes_tabular("./dummy_dir")
+            NumLikesRegression(root="./dummy_dir")
 
     def tearDown(self):
         return super().tearDown()
