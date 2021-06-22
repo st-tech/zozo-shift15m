d13cb71bba61560111268c61d612078cdb1b9630
Masanari Kimura <noconoco.lib@gmail.com>
2021-06-23 00:15:30 +0900

    add target shift

--- shift28m/msgs.py
+++ shift28m/msgs.py
@@ -1,3 +1,4 @@
 DATASET_NOT_FOUND = (
     "Dataset not found or corrupted. You can use download=True to download it."
 )
+INVALID_SHIFT_ARGUMENTS = "You can not specify both covariate_shift and target_shift."
