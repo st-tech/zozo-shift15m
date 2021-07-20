340eea0995556cc0df61df4020a8d81e22fb9fd5
Masanari Kimura <noconoco.lib@gmail.com>
2021-07-20 22:54:10 +0900

    add sum_prices_regression

--- shift28m/datasets/__init__.py
+++ shift28m/datasets/__init__.py
@@ -1,3 +1,5 @@
 from shift28m.datasets.numlikes_tabular import NumLikesRegression
+from shift28m.datasets.sumprices_tabular import SumPricesRegression
 
 assert NumLikesRegression is not None
+assert SumPricesRegression is not None
--- shift28m/datasets/sumprices_tabular.py
+++ shift28m/datasets/sumprices_tabular.py
@@ -10,7 +10,7 @@ from shift28m import msgs as M
 from shift28m.datasets import df_manipulations
 
 
-class NumLikesRegression(object):
+class SumPricesRegression(object):
     def __init__(self, root: str = C.ROOT, load_jsonl: bool = False):
         if load_jsonl:
             # load *.jsonl files in the root directory
@@ -21,29 +21,23 @@ class NumLikesRegression(object):
             self.df: pd.DataFrame = pd.read_json(
                 self.jsonls[0], orient=C.RECORDS, lines=True
             )
+
             for jsonl in self.jsonls[1:]:
                 df_: pd.DataFrame = pd.read_json(jsonl, orient=C.RECORDS, lines=True)
                 self.df = pd.concat([self.df, df_])
 
-            self.df: pd.DataFrame = self.df.sort_values(C.Keys.PUBLISH_DATE)
+            y = []
+            items_list = self.df[C.Keys.ITEMS]
+            for items in items_list:
+                sum_prices = 0.0
+                for item in items:
+                    sum_prices += float(item[C.Keys.PRICE])
+                y.append(sum_prices)
 
-            self.__prepare_features()
-
-            self.y: np.ndarray = np.array(self.df.like_num)
-            self.x: np.ndarray = np.array(
-                (
-                    self.user_ids,
-                    self.price_sum,
-                    self.price_mean,
-                    self.price_max,
-                    self.price_min,
-                )
-            )
-
-            self.x: np.ndarray = self.x.T
-            self.x: np.ndarray = np.hstack([self.x, np.stack(self.category_ids_1)])
+            self.y: np.ndarray = np.array(y)
+            self.x: np.ndarray = np.array(self.df.like_num)
         else:
-            root = os.path.join(root, C.Tasks.NUM_LIKES_REGRESSION)
+            root = os.path.join(root, C.Tasks.SUM_PRICES_REGRESSION)
             # load *.pickle files in the root directory
             self.pickles: list = sorted(glob.glob(os.path.join(root, f"*.{C.PICKLE}")))
             if len(self.pickles) == 0:
@@ -58,33 +52,11 @@ class NumLikesRegression(object):
                     self.x = np.vstack([self.x, x_])
                     self.y = np.hstack([self.y, y_])
 
-    def __prepare_features(self):
-        # generate input features
-        self.user_ids: np.ndarray = (
-            self.df[C.Keys.USER].map(df_manipulations.extract_user_id).values
-        )
-        self.price_sum: np.ndarray = (
-            self.df[C.Keys.ITEMS].map(df_manipulations.price_sum).values
-        )
-        self.price_mean: np.ndarray = (
-            self.df[C.Keys.ITEMS].map(df_manipulations.price_mean).values
-        )
-        self.price_max: np.ndarray = (
-            self.df[C.Keys.ITEMS].map(df_manipulations.price_max).values
-        )
-        self.price_min: np.ndarray = (
-            self.df[C.Keys.ITEMS].map(df_manipulations.price_min).values
-        )
-        self.category_ids_1: np.ndarray = (
-            self.df[C.Keys.ITEMS]
-            .map(df_manipulations.categories_count_embedding_id1)
-            .to_numpy()
-        )
-
     def load_dataset(
         self,
         train_size: int = 10000,
         test_size: int = 10000,
+        covariate_shift: bool = False,
         target_shift: bool = False,
         train_mu: float = 50,
         train_sigma: float = 10,
@@ -94,15 +66,18 @@ class NumLikesRegression(object):
         max_iter: int = 100,
     ):
 
+        if target_shift and covariate_shift:
+            raise RuntimeError(M.INVALID_SHIFT_ARGUMENTS)
+
         N = len(self.x)
-        if not target_shift:
+        if not target_shift and not covariate_shift:
             x_train, x_pool, y_train, y_pool = train_test_split(
                 self.x, self.y, train_size=train_size, random_state=random_seed
             )
             _, x_test, _, y_test = train_test_split(
                 x_pool, y_pool, test_size=test_size, random_state=random_seed
             )
-        else:
+        elif target_shift:
             np.random.seed(random_seed)
             ind = np.argsort(self.y).astype(np.uint32)
             x = self.x[ind]
@@ -127,6 +102,35 @@ class NumLikesRegression(object):
                 train_ind_mu, train_ind_sigma, train_size
             ).astype(np.uint32)
             train_ind = np.delete(train_ind, np.where(train_ind >= len(y))[0], axis=0)
+
+            x_train = x[train_ind]
+            y_train = y[train_ind]
+
+        elif covariate_shift:
+            np.random.seed(random_seed)
+            ind = np.argsort(self.x).astype(np.uint32)
+            x = self.x[ind]
+            y = self.y[ind]
+
+            test_ind_mu = np.searchsorted(x, test_mu)
+            test_ind_sigma = test_ind_mu - np.searchsorted(x, test_mu - test_sigma)
+            test_ind = np.random.normal(test_ind_mu, test_ind_sigma, test_size).astype(
+                np.uint32
+            )
+            test_ind = np.delete(test_ind, np.where(test_ind >= len(y))[0], axis=0)
+
+            x_test = x[test_ind]
+            y_test = y[test_ind]
+
+            x = np.delete(x, test_ind, axis=0)
+            y = np.delete(y, test_ind, axis=0)
+
+            train_ind_mu = np.searchsorted(x, train_mu)
+            train_ind_sigma = train_ind_mu - np.searchsorted(x, train_mu - train_sigma)
+            train_ind = np.random.normal(
+                train_ind_mu, train_ind_sigma, train_size
+            ).astype(np.uint32)
+            train_ind = np.delete(train_ind, np.where(train_ind >= len(x))[0], axis=0)
 
             x_train = x[train_ind]
             y_train = y[train_ind]
