92863a9ae995f9a6303f2a1f34097301481ad936
Masanari Kimura <noconoco.lib@gmail.com>
2021-07-20 23:11:23 +0900

    lint

--- shift28m/constants.py
+++ shift28m/constants.py
@@ -12,6 +12,7 @@ class Keys:
     PRICE = "price"
     PUBLISH_DATE = "publish_date"
 
+
 class Tasks:
     NUM_LIKES_REGRESSION = "num_likes_regression"
     SUM_PRICES_REGRESSION = "sum_prices_regression"
