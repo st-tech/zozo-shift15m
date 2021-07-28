b01b7cec0464084b4f70d4bc49455c05b058b9bd
Masanari Kimura <noconoco.lib@gmail.com>
2021-07-28 20:25:23 +0900

    add msg

--- shift28m/msgs.py
+++ shift28m/msgs.py
@@ -2,3 +2,4 @@ DATASET_NOT_FOUND = (
     "Dataset not found or corrupted. You can use download=True to download it."
 )
 INVALID_SHIFT_ARGUMENTS = "You can not specify both covariate_shift and target_shift."
+UNSUPPORTED_SHIFT_TYPE = "This dataset does not support specified shift type."
