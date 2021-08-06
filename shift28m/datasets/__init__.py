ca8c4f510510c9caf9adb9e7a62557594b7d585c
Masanari Kimura <noconoco.lib@gmail.com>
2021-08-06 12:41:59 +0900

    28M -> 15M: shift15m

--- shift28m/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from shift28m.datasets.numlikes_tabular import NumLikesRegression
-from shift28m.datasets.sumprices_tabular import SumPricesRegression
-
-assert NumLikesRegression is not None
-assert SumPricesRegression is not None
