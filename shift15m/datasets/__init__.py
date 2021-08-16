37ebb8c4339fe9d09f8218855a5befde57f4979e
tn1031 <ttt.nakamura1031@gmail.com>
2021-08-16 04:01:17 +0000

    run format

--- shift15m/datasets/__init__.py
+++ shift15m/datasets/__init__.py
@@ -3,7 +3,9 @@ from shift15m.datasets.sumprices_tabular import SumPricesRegression
 
 try:
     from shift15m.datasets.imagefeature_torch import ImageFeatureDataset
-    from shift15m.datasets.imagefeature_torch import get_loader as get_imagefeature_dataloader
+    from shift15m.datasets.imagefeature_torch import (
+        get_loader as get_imagefeature_dataloader,
+    )
 
     assert ImageFeatureDataset is not None
     assert get_imagefeature_dataloader is not None
