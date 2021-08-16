faa573a2c8502f9a4a278da40c91b889093cf717
tn1031 <ttt.nakamura1031@gmail.com>
2021-08-16 04:32:17 +0000

    fix testcase

--- tests/datasets/test_imagefeature_torch.py
+++ tests/datasets/test_imagefeature_torch.py
@@ -1,23 +1,34 @@
+import gzip
 import pathlib
+import shutil
 import unittest
 
-import shift15m.constants as C
-from shift15m.datasets import ImageFeatureDataset
+try:
+    from shift15m.datasets import ImageFeatureDataset
+
+    SKIP = False
+except ImportError:
+    SKIP = True
 
 
 class TestNumlikesTabular(unittest.TestCase):
     def setUp(self):
+        pathlib.Path("test_features").mkdir(exist_ok=True)
+        with gzip.open("test_features/xxxxx.json.gz", mode="wt") as fp:
+            fp.write("[1, 2, 3]")
         return super().setUp()
 
+    @unittest.skipIf(SKIP, "not supported")
     def test_dataset_corrupted(self):
-        items = [("1000025", "10")]
-        dataset = ImageFeatureDataset(items, pathlib.Path(C.FEATURE_ROOT))
+        items = [("xxxxx", "10")]
+        dataset = ImageFeatureDataset(items, pathlib.Path("test_features"))
 
         vec, label = dataset[0]
-        self.assertEqual(len(vec), 4096)
+        self.assertEqual(len(vec), 3)
         self.assertEqual(label, 0)
         self.assertEqual(dataset.category_size, 1)
         self.assertEqual(dataset.category_count[0], 1)
 
     def tearDown(self):
+        shutil.rmtree("test_features")
         return super().tearDown()
