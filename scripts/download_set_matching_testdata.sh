666db4ac9629c21254c3490527dfe12ef71ff6c1
tn1031 <ttt.nakamura1031@gmail.com>
2021-10-21 05:05:05 +0000

    fix download dir

--- scripts/download_set_matching_testdata.sh
+++ scripts/download_set_matching_testdata.sh
@@ -6,5 +6,5 @@ readonly TASK_ROOT=data/set_matching
 
 mkdir -p ${TASK_ROOT}
 wget -i https://research.zozo.com/data_release/shift15m/set_matching/set_matching_test_data_splits/filelist.txt -P ${TASK_ROOT}/set_matching_test_data_splits
-cat inputs/set_matching/set_matching_test_data_splits/set_matching_test_data.tar.gz-* > ${TASK_ROOT}/set_matching_test_data.tar.gz
-tar zxvf inputs/set_matching/set_matching_test_data.tar.gz -C ${TASK_ROOT}
+cat ${TASK_ROOT}/set_matching_test_data_splits/set_matching_test_data.tar.gz-* > ${TASK_ROOT}/set_matching_test_data.tar.gz
+tar zxvf ${TASK_ROOT}/set_matching_test_data.tar.gz -C ${TASK_ROOT}
